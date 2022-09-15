cacbf1fe183c68ef62f168f848737132518e562c
Martha A <mladame43@gmail.com>
2022-09-14 18:24:43 -0700

    downloaded inquirer

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // WHEN I am prompted for my team members and their information
 // THEN an HTML file is generated that displays a nicely formatted team roster based on user input
 // WHEN I click on an email address in the HTML
-// THEN my default email program opens and populates the TO field of the email with the address
+// THEN my default email program opens and populates the TO field of the email with the address (mail to)
 // WHEN I click on the GitHub username
 // THEN that GitHub profile opens in a new tab
 // WHEN I start the application
