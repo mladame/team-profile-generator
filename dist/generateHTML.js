b2742d18d8342d856e134bbefe0a1043c74e97ea
Martha A <mladame43@gmail.com>
2022-09-17 16:36:55 -0700

    added initial function to render manager card, added template literals

--- dist/generateHTML.js
+++ dist/generateHTML.js
@@ -4,11 +4,31 @@
 //todo: make function that creates github link
 function renderGithubLink(){
 
+
 }
 
 //todo: make function that generates manager card
 function renderManagerCard(){
 
+    for (const member of teamMembers) {
+        if (member.role === "Manager"){
+            return `
+
+            <div class="card border-dark mb-3" style="max-width: 18rem;">
+            <div class="card-header my-2">
+                <h2>${member.name}</h2>
+                <h3>Manager <i class="fa-solid fa-mug-saucer"></i></h3>
+            </div>
+            <div class="card-body text-dark">
+                <h5 class="card-title my-3">ID: ${member.id}</h5>
+                <h5 class="card-title my-3"><a href="mailto:${member.email}">Send Email</a></h5>
+                <h5 class="card-title my-3">Office Number: ${member.office}</h5>
+            </div>
+        </div>
+        
+            `
+        }
+    }
 }
 
 
